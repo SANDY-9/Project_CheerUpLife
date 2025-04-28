a56bbe6c4e12c736bbb96a16c8f2a2453523e741
SANDY <nnal0256@naver.com>
2025-04-29 02:52:51 +0900

    rename: MainActivity -> ui 패키지로 이동

--- app/src/main/java/com/sandy/cheeruplife/ui/MainActivity.kt
+++ app/src/main/java/com/sandy/cheeruplife/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sandy.cheeruplife
+package com.sandy.cheeruplife.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.sandy.cheeruplife.ui.theme.CheerUpLifeTheme
+import com.cheeruplife.core.designsystem.theme.CheerUpLifeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
