5aaf22aad971ec62c72206eb79f152c87f092128
SANDY <nnal0256@naver.com>
2025-04-29 05:49:48 +0900

    add: core:data 모듈 추가

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.cheeruplife.core.data"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:domain"))
+    implementation(project(":core:model"))
+
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+}
