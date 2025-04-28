54a030cab0ab7b4aee439bd79b30f5bffce4baf5
SANDY <nnal0256@naver.com>
2025-04-28 13:28:17 +0900

    add: compose 라이브러리 추가

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,13 +1,30 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+
+android {
+    namespace = "com.sandy.cheeruplife"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+        targetSdk = 35
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
 }
