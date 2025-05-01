b4735b501468bea8736370e36a00e6a042a230b3
SANDY <nnal0256@naver.com>
2025-05-02 02:15:41 +0900

    add: :core:network 모듈 추가

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.cheeruplife.core.data"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(project(":core:domain"))
+    implementation(project(":core:model"))
+
+    implementation(libs.bundles.hilt)
+    ksp(libs.hilt.compiler)
+
+    implementation(libs.bundles.retrofit)
+    implementation(libs.kotlinx.serialization)
+
+}
