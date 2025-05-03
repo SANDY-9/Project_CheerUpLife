995e6d94ce742b5cd8c3fe07e5f1e3bf17259e37
SANDY <nnal0256@naver.com>
2025-05-03 13:08:37 +0900

    feat: Calendar 생성 유틸 개발

---
+++ core/calendar/src/main/java/com/cheeruplife/core/calendar/model/Calendar.kt
@@ -0,0 +1,36 @@
+package com.cheeruplife.core.calendar.model
+
+import com.cheeruplife.core.calendar.utils.CalendarUtils
+import com.cheeruplife.core.model.Date
+import com.cheeruplife.core.model.enums.DayOfWeek
+
+typealias Week = List<Date>
+
+data class Calendar(
+    val days: List<Week>,
+    val month: Int,
+    val year: Int,
+) {
+    companion object {
+        fun create(
+            year: Int,
+            month: Int,
+        ): Calendar {
+            return Calendar(
+                year = year,
+                month = month,
+                days = CalendarUtils.getMonthByWeek(year, month)
+                    .map { week ->
+                        week.map { localDate ->
+                            Date(
+                                year = localDate.year,
+                                month = localDate.monthValue,
+                                day = localDate.dayOfMonth,
+                                dayOfWeek = DayOfWeek.get(localDate.dayOfWeek.value),
+                            )
+                        }
+                    }
+            )
+        }
+    }
+}
